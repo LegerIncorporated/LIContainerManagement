20882c746a9e00350d2c57ba58244433c39f6240
Justin Leger <justin.leger@wnco.com>
2022-08-12 21:38:30 -0500

    Added empty package files to hide the sample project.

---
+++ Example/Package.swift
@@ -0,0 +1,13 @@
+// swift-tools-version: 5.6
+
+// WORKAROUND FOR NOT SHOWING EXAMPLE FILES IN THE SWIFT PACKAGE.
+
+
+import PackageDescription
+
+let package = Package(
+    name: "",
+    products: [],
+    dependencies: [],
+    targets: []
+)
